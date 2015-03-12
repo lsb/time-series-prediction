58b5e01699f29c509f2874d63ca605897c8b9172
lsb <leebutterman@gmail.com>
2015-03-11 20:53:25 -0700

    update schema

--- project.clj
+++ project.clj
@@ -4,10 +4,9 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [protobuf "0.6.2"]
-                 #_[com.google.protobuf/protobuf-java "2.5.0"]
+                 [org.flatland/protobuf "0.8.1"]
                  [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-protobuf "0.4.1" :exclusions [leinjacker org.clojure/clojure]]
+  :plugins [[lein-protobuf "0.4.1"]
             [lein-ring "0.8.13" :exclusions [org.clojure/clojure]]]
   :ring {:handler time-series-prediction.handler/app :init time-series-prediction.handler/async}
   :profiles
